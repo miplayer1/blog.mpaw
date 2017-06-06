e5bbdf51ccce06ffecc9ab82c9cab792e759abec
mi <mi@localhost.localdomain>
2017-06-06 18:22:55 +0200

    mise en forme main+footer

--- footer.php
+++ footer.php
@@ -6,16 +6,17 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/main.js"></script>
     <footer class="panel-footer text-center col-sm-12">
-    		 <div>
+		<section>		 		
+    		 		<div>
                     <a href="http://blog.mpaw.xyz" target="_blank" title="Rejoindre le blog">Le blog</a>
                 </div>
                 <div> | </div>
                 <div>
-                    <a href="https://www.facebook.com/MaPetiteAgenceWeb/" target="_blank"><img src="public/images/logoFB.svg" width="40rem" alt="Suivez-nous sur facebook" /></a>
+                    <a href="https://www.facebook.com/MaPetiteAgenceWeb/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logoFB.svg" width="40rem" alt="Suivez-nous sur facebook" /></a>
                 </div>
-            </section>
-            <p><img src="public/images/pinapple-icon.png" width="20rem">
-            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a> - 2017 | Réalisé par <a href="http://www.cooldev.xyz">Cooldev</a><img src="public/images/pinapple-icon.png" width="20rem"></br>
+       </section>    
+            <p><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pinapple-icon.png" width="20rem">
+            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a> - 2017 | Réalisé par <a href="http://www.cooldev.xyz">Cooldev</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pinapple-icon.png" width="20rem"></br>
             <a href="ml.php">Mentions légales</a></p>
         <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="blank">CC BY-NC-SA 4.0</a> - 2017 <a href="http://www.mpaw.xyz" target="blank">MPAW</a> 
     </footer>
