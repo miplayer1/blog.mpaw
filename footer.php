7fa2516e5bdf00ab8fbb2cf2eaeb45b4e1fba84b
mi <mi@localhost.localdomain>
2017-06-06 17:08:22 +0200

    mise en forme main+footer

--- footer.php
+++ footer.php
@@ -6,7 +6,18 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/main.js"></script>
     <footer class="panel-footer text-center col-sm-12">
-        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="blank">CC BY-NC-SA 4.0</a> - 2017 <a href="http://www.mpaw.xyz" target="blank">MPAW</a>
+    		 <div>
+                    <a href="http://blog.mpaw.xyz" target="_blank" title="Rejoindre le blog">Le blog</a>
+                </div>
+                <div> | </div>
+                <div>
+                    <a href="https://www.facebook.com/MaPetiteAgenceWeb/" target="_blank"><img src="public/images/logoFB.svg" width="40rem" alt="Suivez-nous sur facebook" /></a>
+                </div>
+            </section>
+            <p><img src="public/images/pinapple-icon.png" width="20rem">
+            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a> - 2017 | Réalisé par <a href="http://www.cooldev.xyz">Cooldev</a><img src="public/images/pinapple-icon.png" width="20rem"></br>
+            <a href="ml.php">Mentions légales</a></p>
+        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="blank">CC BY-NC-SA 4.0</a> - 2017 <a href="http://www.mpaw.xyz" target="blank">MPAW</a> 
     </footer>
     <?php wp_footer(); ?>
   </body>
